4bbe179448a739b43f1fa40238bd6bbd91729176
dyegolara <dyegolara@icloud.com>
2024-08-11 15:14:21 -0600

    add favicon and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,8 @@ import Header from "@/components/Header";
 
 export const metadata: Metadata = {
   title: "Tornillos Barajas",
-  description: "Tornillos y Remaches",
+  description:
+    "Tornillos y Remaches Barajas se pone a su disposición para el diseño y desarrollo de cualquier tornillo o artículo para solucionar su problema de fijación, contamos con más de 15 años de presencia en la Industria Metal-Mecánica.",
 };
 
 const bodyStyle = { backgroundColor: "var(--color-background)", margin: 0 };
