743151a8ba93fcfac87a7e7fe662461f1dfa07ad
dyegolara <dyegolara@icloud.com>
2024-08-13 12:37:22 -0600

    add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "@radix-ui/themes/styles.css";
 
-import { Container } from "@radix-ui/themes";
+import { Box, Container, Flex, Text } from "@radix-ui/themes";
 import type { Metadata } from "next";
 
 import { Providers } from "@/app/providers";
@@ -12,8 +12,6 @@ export const metadata: Metadata = {
     "Tornillos y Remaches Barajas se pone a su disposición para el diseño y desarrollo de cualquier tornillo o artículo para solucionar su problema de fijación, contamos con más de 15 años de presencia en la Industria Metal-Mecánica.",
 };
 
-const bodyStyle = { backgroundColor: "var(--color-background)", margin: 0 };
-
 export default function RootLayout({
   children,
 }: {
@@ -21,12 +19,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body style={bodyStyle}>
+      <body
+        style={{
+          backgroundColor: "var(--color-background)",
+          margin: 0,
+          minHeight: "100vh",
+        }}
+      >
         <Providers>
-          <Header />
-          <Container size="3" p="4">
-            {children}
-          </Container>
+          <Flex direction={"column"} height={"100vh"}>
+            <Header />
+            <Container size="3" p="4" flexGrow={"1"}>
+              {children}
+            </Container>
+            <Flex
+              justify={"center"}
+              p={"6"}
+              style={{ borderTop: "1px solid var(--gray-5)" }}
+            >
+              <Text>
+                &copy; {new Date().getFullYear()} Tornillos y Remaches Barajas
+                S.A de C.V. Todos los derechos reservados.
+              </Text>
+            </Flex>
+          </Flex>
         </Providers>
       </body>
     </html>
